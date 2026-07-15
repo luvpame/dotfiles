32d16cdb1756b291b903cb283687f82648920a3f
luvpame <ayuu.kumakuma@gmail.com>
2026-07-15 13:31:49 +0900

    chore(nvim): pr.nvim を削除してプラグインを更新

--- config/nvim/lua/plugins/snacks.nvim.lua
+++ config/nvim/lua/plugins/snacks.nvim.lua
@@ -81,7 +81,6 @@ return {
             desc = "Config",
             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
           },
-          { icon = " ", key = "p", desc = "Pull Requests", action = ":PR" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
           { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
