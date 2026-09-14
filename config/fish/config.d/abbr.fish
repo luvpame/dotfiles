e6510e972b47f75b01ccde1691f312e1b5f06626
luvpame <ayuu.kumakuma@gmail.com>
2026-09-14 09:18:30 +0900

    feat(lazygit): switch terminal shortcuts to lazygit

--- config/fish/config.d/abbr.fish
+++ config/fish/config.d/abbr.fish
@@ -13,7 +13,7 @@ if status is-interactive
     abbr -a cdg cd-gitroot
     abbr -a cat bat
     abbr -a co 'cage codex'
-    abbr -a lg ziggity
+    abbr -a lg lazygit
     abbr -a tm tmux
     abbr -a hd herdr
 end
